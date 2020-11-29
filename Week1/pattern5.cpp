0524b04dd77d28cb5d35b0642061f95f18aa7c60
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-11-29 15:46:52 +0530

    Create pattern5.cpp

---
+++ Week1/pattern5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for(char c='A';c<='D';c++)
+	{
+	  for(char d='A';d<='D';d++)
+	  {
+	      cout<<d;
+	  }
+	  cout<<endl;
+	}
+	return 0;
+}
