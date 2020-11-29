1a25bd906e668370023bdcfc70338588b2100e28
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-11-29 15:45:25 +0530

    Create pattern4.cpp

---
+++ Week1/pattern4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for(char c='A';c<='D';c++)
+	{
+	  for(char d='A';d<='D';d++)
+	  {
+	      cout<<c;
+	  }
+	  cout<<endl;
+	}
+	return 0;
+}
