df02a5c353ad6a18bb4db7220065dcad5d7f5b69
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-11-29 15:59:40 +0530

    Create pattern14.cpp

---
+++ Week1/pattern14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for(char i='A';i<='E';i++)
+	{
+	  for(char j='A';j<=i;j++)
+	  {
+	      cout<<i;
+	  }
+	  cout<<endl;
+	}
+	return 0;
+}
