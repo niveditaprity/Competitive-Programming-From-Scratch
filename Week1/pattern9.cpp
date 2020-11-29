c28488b4982605036716e7d4a9b9b64d0313cb67
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-11-29 15:52:36 +0530

    Create pattern9.cpp

---
+++ Week1/pattern9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for(char i='E';i>='A';i--)
+	{
+	  for(char c='E';c>='A';c--)
+	  {
+	      cout<<i;
+	  }
+	  cout<<endl;
+	}
+	return 0;
+}
