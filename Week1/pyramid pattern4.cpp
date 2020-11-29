ea5d938be51873a8eb3f35223f2782369ca015e7
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-11-29 22:04:12 +0530

    Create pyramid pattern4.cpp

---
+++ Week1/pyramid pattern4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int px=n-1;
+    int py=n-1;
+     char k='A';
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<2*n-1;j++)
+        {
+          if(j>=px&&j<=py)
+          {
+
+              cout<<k;
+          }
+          else
+          {
+              cout<<" ";
+          }
+        }
+        k++;
+        px--;
+        py++;
+        cout<<endl;
+    }
+	return 0;
+}
