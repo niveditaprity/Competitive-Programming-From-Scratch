2dc19d202bbe0bcf89eaf61afa92a08c717fff31
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-11-29 19:15:51 +0530

    Create pattern21.cpp

---
+++ Week1/pattern21.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for(int i=1;i<=5;i++)
+	{
+	  for(int j=5;j>=1;j--)
+	  {
+	   if(i<j)
+	   {
+	       cout<<" ";
+	   }
+	   else
+	   {
+	       cout<<j;
+	   }
+	      
+	  }
+	  cout<<endl;
+	}
+	return 0;
+}
