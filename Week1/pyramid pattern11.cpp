e02334e209e971dd536aa78a161aa1ba2d19864e
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-11-30 10:57:39 +0530

    Create pyramid pattern11.cpp

---
+++ Week1/pyramid pattern11.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int z=0;
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n-1;j>i;j--)
+        {
+              cout<<" ";
+        }
+        for(int k=0;k<=c;k++)
+        {
+
+                printf("%c",abs(z-k)+65);
+        }
+        c+=2;
+        z+=1;
+        
+        cout<<endl;
+    }
+	return 0;
+}
