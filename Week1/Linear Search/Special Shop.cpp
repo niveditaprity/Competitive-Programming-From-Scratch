785c127d81433e22c76a54088176948ea01e14c4
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-12-03 15:25:57 +0530

    Update Special Shop.cpp

--- Week1/Linear Search/Special Shop.cpp
+++ Week1/Linear Search/Special Shop.cpp
@@ -10,12 +10,17 @@ int main()
     int n,a,b;
    cin>>n>>a>>b;
     int m=INT_MAX;
-    for(int i=0;i<=n;i++)
+    for(int i=0;i<=n/2;i++)
     {
       int p=i*i*a+(n-i)*(n-i)*b;
       if(p<m)
       {
           m=p;
+      }
+       int x=i*i*b+(n-i)*(n-i)*a;
+        if(x<m)
+      {
+          m=x;
       }
     }
         cout<<m<<endl;
