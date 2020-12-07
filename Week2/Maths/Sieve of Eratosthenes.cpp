cea9bb791048e4d4a8d8595e6c1f6f8e1e1a9c91
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-12-07 12:35:59 +0530

    Create Sieve of Eratosthenes.cpp

---
+++ Week2/Maths/Sieve of Eratosthenes.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n+1];
+    for(int i=0;i<=n;i++)
+    {
+        arr[i]=1;
+    }
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(arr[i]==1)
+        {
+            cout<<i<<" ";
+            for(int j=i*i;j<=n;j+=i)
+            {
+                arr[j]=0;
+            }
+        }
+    }
+    for(int j=sqrt(n)+1;j<=n;j++)
+    {
+        if(arr[j]==1)
+        {
+            cout<<j<<" ";
+        }
+    }
+	return 0;
+}
