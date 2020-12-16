c510b0c4c632a7898bb6df187c90f3223850461f
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-12-16 15:12:49 +0530

    https://leetcode.com/problems/guess-number-higher-or-lower/

---
+++ Week2/Binary Search/374. Guess Number Higher or Lower.cpp
@@ -0,0 +1,31 @@
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low=1;
+        int high=n;
+        
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            
+            int res=guess(mid);
+            
+            if(res==0)
+            {
+                return mid;
+            }
+            else if(res==-1)
+            {
+                high=mid;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        
+        return 0;
+        
+    }
+};
