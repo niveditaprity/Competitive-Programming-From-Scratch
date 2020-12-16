bd21984d75c13bfc29734d47adf36ec97b745366
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-12-16 13:44:17 +0530

    Create 278. First Bad Version.cpp
    Leetcode

---
+++ Week2/Binary Search/278. First Bad Version.cpp
@@ -0,0 +1,23 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l=1,r=n;
+        while(l<r)
+        {
+            int mid=l+(r-l)/2;
+            if(isBadVersion(mid)==false)
+            {
+                l=mid+1;
+            }
+            if(isBadVersion(mid)==true)
+            {
+                r=mid;
+            }
+            
+        }
+        return l;
+    }
+};
