79186f500239bef6b00231f5e0384d15478c6326
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2021-04-25 10:44:32 +0530

    Create Trial division.cpp

---
+++ cp-algorithm/Algebra/Integer factorization/Trial division.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main() {
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+        vector<ll>factorisation;
+        for(ll i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                factorisation.push_back(i);
+                n=n/i;
+            }
+        }
+        if (n > 1)
+        factorisation.push_back(n);
+        for(ll i=0;i<factorisation.size();i++)
+        {
+            cout<<factorisation[i]<<" ";
+        }
+        cout<<"\n";
+    }
+	return 0;
+}
